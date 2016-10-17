c7ea5d31a94c3a64d58d83c04a727faa373026a1
Arturo Gonzalez <arturogonzalez@Arturos-MacBook-Pro.local>
2016-10-17 16:57:05 +0200

    customised formatting

---
+++ users/formats.py
@@ -0,0 +1,9 @@
+"""
+Custom Formats
+"""
+
+# Get rid of unwanted characters in name and surname
+name_format = '/><=+-?!#"  "$%^&*""()" "_+:;'
+
+# Validate email format
+email_format = '^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$'
